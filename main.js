f582556899ef4138288bfe2ab53a5214141eff42
Gabor Berta <shraatz@gmail.com>
2013-08-20 22:12:26 +0200

    skills animation + refining the movements

--- main.js
+++ main.js
@@ -8,17 +8,20 @@ var SiteHandler ={
         pagesClass:         'step'
     },
     
-    currentStep:    0,
+    currentStep:            0,
+    currentlyIsAnimating:   false,
+    finishedAnimations:     [],
     
     init:           function(options){
         SiteHandler.settings    =$.extend(SiteHandler.settings, options);
         SiteHandler.listenScrolling();
+        SiteHandler.domReadyEvents();
         return SiteHandler;
     },
     
     wheel:          function(e){
         e.preventDefault();
-        if(Math.abs(e.wheelDeltaY)>=50){
+        if(!SiteHandler.currentlyIsAnimating && Math.abs(e.wheelDeltaY)>=1){
             SiteHandler.step(e.wheelDeltaY);
         }
     },
@@ -28,27 +31,41 @@ var SiteHandler ={
     },
     
     step:           function(way){
-        SiteHandler.ignoreScrolling();
+        //SiteHandler.ignoreScrolling();
         var maxElements =$('.'+SiteHandler.settings.pagesClass).length;
         
         if(way<0 && SiteHandler.currentStep!=(maxElements-1)){
+            SiteHandler.currentlyIsAnimating=   true;
             $('.'+SiteHandler.settings.pagesClass+':eq('+SiteHandler.currentStep+')').animate({top:'-100%'},SiteHandler.settings.animSpeed,SiteHandler.settings.fx,SiteHandler.stepAnimation);
             SiteHandler.currentStep++;
         }
         else if(way>0 && SiteHandler.currentStep!=1){
+            SiteHandler.currentlyIsAnimating=   true;
             $('.'+SiteHandler.settings.pagesClass+':eq('+(SiteHandler.currentStep-1)+')').animate({top:'0%'},SiteHandler.settings.animSpeed,SiteHandler.settings.fx,SiteHandler.stepAnimation);
             SiteHandler.currentStep--;
         }
         
-        SiteHandler.currentStep     =(SiteHandler.currentStep<0)?0:(SiteHandler.currentStep>maxElements)?maxElements:SiteHandler.currentStep;
-        setTimeout(SiteHandler.listenScrolling,SiteHandler.settings.sleep);
+        SiteHandler.currentStep             =(SiteHandler.currentStep<0)?0:(SiteHandler.currentStep>maxElements)?maxElements:SiteHandler.currentStep;
+        //setTimeout(SiteHandler.listenScrolling,SiteHandler.settings.sleep);
     },
     
     stepAnimation:  function(){
+        if(SiteHandler.finishedAnimations.indexOf(SiteHandler.currentStep)!=-1)
+            return SiteHandler.allAnimationsHasBeenFinished();
         switch(SiteHandler.currentStep){
             case 1:SiteHandler.aboutAnim();break;
+            case 2:SiteHandler.skillsAnim();break;
+            default:SiteHandler.allAnimationsHasBeenFinished();break;
         }
-        console.log(SiteHandler.currentStep);
+    },
+    
+    animateArrow:   function(element){
+        $(element).animate({bottom:'10%',opacity:1},SiteHandler.settings.animSpeed,SiteHandler.settings.fx);
+    },
+    
+    skillsAnim:     function(){
+        $('#skills .skillsCircle').animate({top:'150px'},SiteHandler.settings.animSpeed,SiteHandler.settings.fx);
+        SiteHandler.chainingSkillsAnimation();
     },
     
     aboutAnim:      function(){
@@ -56,7 +73,9 @@ var SiteHandler ={
         $('header').animate({backgroundPositionY:'20px'},SiteHandler.settings.animSpeed,SiteHandler.settings.fx,function(){
             $('#about .welcomeCircle').animate({top:'150px'},SiteHandler.settings.animSpeed,SiteHandler.settings.fx);
             $('#about .welcomeMessage').animate({bottom:'20%',width:'600px',marginLeft:'-300px',height:'130px'},SiteHandler.settings.animSpeed,SiteHandler.settings.fx);
+            SiteHandler.animateArrow('#about .arrow');
             SiteHandler.chainingSocialAnimations(($('header > a').length-1));
+            $('header a').promise().done(SiteHandler.allAnimationsHasBeenFinished);
         });
     },
     
@@ -67,6 +86,35 @@ var SiteHandler ={
                 SiteHandler.chainingSocialAnimations((idx-1));
             });
         }
+    },
+    
+    chainingSkillsAnimation:    function(){
+        var middlePoint =Math.floor($('#skills .skillsGroup').length/2);
+        SiteHandler.skillsAnimationToDirection(-1,middlePoint-1,0);
+        SiteHandler.skillsAnimationToDirection(1,middlePoint,0);
+    },
+    
+    skillsAnimationToDirection:      function(direction,idx,itemsAnimated){
+        var element =$('#skills .skillsGroup:eq('+idx+')');
+        if(element.length>0){
+            element.animate({bottom:0},SiteHandler.settings.animSpeed/4,function(){
+                SiteHandler.skillsAnimationToDirection(direction,idx+direction,itemsAnimated+1);
+            });
+        }
+        if(itemsAnimated == $('#skills .skillsGroup').length)
+            SiteHandler.moveSkillsGroupHigher();
+    },
+    
+    moveSkillsGroupHigher:          function(){
+        $('#skills .skillsDetail').css({display:'inline-block'});
+        $('#skills .skillsContainer.group').animate({bottom:'200px'},SiteHandler.settings.animSpeed/4);
+        $('#skills .skillsCircle').animate({width:'100px',height:'100px',lineHeight:'100px',fontSize:'18px'},SiteHandler.settings.animSpeed/4,SiteHandler.allAnimationsHasBeenFinished);
+    },
+    
+    allAnimationsHasBeenFinished:   function(){
+        SiteHandler.currentlyIsAnimating=   false;
+        SiteHandler.finishedAnimations.push(SiteHandler.currentStep);
+        console.log('done');
     },
     
     listenScrolling:    function(){
@@ -83,6 +131,13 @@ var SiteHandler ={
             window.removeEventListener('touchmove', SiteHandler.touch, false);
         }
         window.onmousewheel = document.onmousewheel = null;  
+    },
+    
+    domReadyEvents:     function(){
+        $(function(){
+            SiteHandler.animateArrow('#preload .arrow');
+        });
     }
+    
     
 };
